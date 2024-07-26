a4a781765fbb263b0655f472f4e32de24bec643a
sedentaryowl <sedentaryowl@riseup.net>
2024-07-26 20:12:31 +0800

    running backups once per day with notification warnings

--- backdots/main.c
+++ backdots/main.c
@@ -1,6 +1,10 @@
-#define _DEFAULT_SOURCE
 #include <dirent.h>
+#include <time.h>
+#include <signal.h>
+#include "libnotify/notify.h"
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -9,6 +13,7 @@ char *TARGET = "/home/dell/jit/dotfiles";
 char *TARGET_TEST = "/home/dell/learndev/c/backdots/test/target";
 
 char *list_dir[] = {
+    "/home/dell/.config/pkglist.txt",
     "/home/dell/.config/mpd",
     "/home/dell/.config/ncmpcpp",
     "/home/dell/.config/hypr",
@@ -22,19 +27,61 @@ char *list_dir[] = {
 };
 
 char *list_test[] = {
-  "/home/dell/learndev/c/backdots/test/dummy/mpd",
-  "/home/dell/learndev/c/backdots/test/dummy/ncmpcpp",
-  "/home/dell/learndev/c/backdots/test/dummy/here.txt",
-  "/home/dell/learndev/c/backdots/test/dummy/hypr",
+    "/home/dell/learndev/c/backdots/test/dummy/mpd",
+    "/home/dell/learndev/c/backdots/test/dummy/ncmpcpp",
+    "/home/dell/learndev/c/backdots/test/dummy/here.txt",
+    "/home/dell/learndev/c/backdots/test/dummy/hypr",
 };
 
-void backup();
-void rec_dir(char *src, char *dst);
-int copy_file(char *source_file, char *dst_file);
+bool is_cancelled = false;
 
-int main() { backup(); }
+void backup();
+void rec_dir(char *dir_src, char *dir_dst);
+int copy_file(char *source_file, char *dst_file);
+NotifyNotification *warn(char *title, char *message, int timeout);
+void log_date();
+bool check_run();
+void signal_handler(int signo);
+
+int main(int argc, char **argv) {
+  signal(SIGINT, signal_handler);
+  signal(SIGHUP, signal_handler);
+
+  if (argc == 2 && !strcmp(argv[1], "daemon")) {
+    printf("starting daemon\n");
+
+    while (true) {
+      if (!check_run()) {
+        printf("already ran today\n");
+      } else {
+        printf("starting backup in 10 minutes\n");
+        NotifyNotification *warnNotif = warn("backdots warning", "Starting dotfiles backup in 10 min\n$(~/bin/canback.sh) to cancel", 1000 * 60 * 5);
+        sleep(10 * 60);
+
+        if (is_cancelled) {
+          notify_notification_close(warnNotif, NULL);
+          warn("backdots", "backup cancelled", 1000 * 5);
+          printf("backup cancelled\n");
+          is_cancelled = false;
+        } else {
+          printf("backup starting...\n");
+          backup();
+          printf("backup complete\n");
+          log_date();
+        }
+      }
+      sleep(6 * 60 * 60);
+    }
+
+    return 0;
+  }
+
+  backup();
+  return 0;
+}
 
 void backup() {
+  system("pacman -Qe > /home/dell/.config/pkglist.txt");
   for (long unsigned i = 0; i < sizeof(list_dir) / sizeof(list_dir[0]); i++) {
     int delim;
 
@@ -115,4 +162,65 @@ int copy_file(char *source_file, char *dst_file) {
   fclose(dst_cp);
 
   return 0;
+}
+
+NotifyNotification *warn(char *title, char *message, int timeout) {
+  notify_init("backdots");
+
+  NotifyNotification *notif;
+
+  notif = notify_notification_new(title,
+                                  message, NULL);
+
+  notify_notification_set_urgency(notif, NOTIFY_URGENCY_LOW);
+
+  notify_notification_set_timeout(notif, timeout);
+
+  notify_notification_show(notif, NULL);
+
+  notify_uninit();
+  return notif;
+}
+
+void log_date() {
+  FILE *log_file = fopen("/home/dell/learndev/c/backdots/date.log", "w");
+
+  struct tm *tm;
+  time_t t = time(NULL);
+  tm = localtime(&t);
+  char date_now[11];
+
+  strftime(date_now, 11, "%d-%m-%Y", tm);
+
+  fputs(date_now, log_file);
+  fclose(log_file);
+}
+
+bool check_run() {
+  FILE *log_file = fopen("/home/dell/learndev/c/backdots/date.log", "r");
+
+  struct tm *tm;
+  time_t t = time(NULL);
+  tm = localtime(&t);
+  char date_now[11];
+  char date_then[11];
+
+  strftime(date_now, 11, "%d-%m-%Y", tm);
+
+  fgets(date_then, 11, log_file);
+  fclose(log_file);
+
+  return strcmp(date_then, date_now);
+}
+
+void signal_handler(int signo) {
+  if (signo == SIGHUP) {
+    printf("received SIGHUP\n");
+    is_cancelled = true;
+  }
+
+  if (signo == SIGINT) {
+    printf("received SIGINT\n");
+    exit(0);
+  }
 }
